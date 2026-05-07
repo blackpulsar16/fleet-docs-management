70bfec6d95d11e455c78e8cb915aaa65fc78bd16
Diego Hurtado <diego.hurtado@tlock.mx>
2026-05-07 16:32:42 -0600

    minio fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    proxy: {
+      '/documents': 'http://localhost:8001',
+      '/vehicles': 'http://localhost:8001',
+      '/fleet': 'http://localhost:8001',
+      '/ingest-documents': 'http://localhost:8001'
+    }
+  }
 })
